0002bbc68d501a9c3d129d6c929a3ee81afb4fe6
SATGE Nathan <Nathan.Satge@bordeaux-inp.fr>
2024-02-24 12:12:45 +0100

    remove

--- src/migrations/2_deploy_contract.js
+++
@@ -1,7 +0,0 @@
-const AgeContrat = artifacts.require("AgeContrat");
-
-module.exports = function (deployer, network, accounts) {
-    const deployerAccount = accounts[1]; // Choisissez le compte que vous souhaitez utiliser
-
-    deployer.deploy(AgeContrat, { from: deployerAccount });
-};
